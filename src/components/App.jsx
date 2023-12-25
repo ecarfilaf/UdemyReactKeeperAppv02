69ee9897eac735dc8c8d0892e40699394c291a84
ecarfilaf <esteban.carfilaf@sonda.com>
2023-12-25 19:11:29 -0300

    Udemy backend v01.002.001 React Keeper App Part 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,15 +3,22 @@ import reactDom from "react-dom";
 import Heading from "./Heading";
 import Note from "./Note";
 import Footer from "./Footer";
+import notes from "../notes";
 
 function App() {
-  return (
-    <div>
-      <Heading />
-      <Note />
-      <Footer />
-    </div>
-  );
+return ( 
+	<div>
+		<Heading />
+		{notes.map((noteItem) => (
+			 <Note		
+			key={noteItem.key}
+			title={noteItem.title}
+			content={noteItem.content}
+			/>
+		))}
+		<Footer />
+	</div>
+);
 }
 
 export default App;
