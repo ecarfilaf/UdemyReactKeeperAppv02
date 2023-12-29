f43713282055cc1fd2f0793f49b7a52fde84e0b9
ecarfilaf <esteban.carfilaf@sonda.com>
2023-12-28 23:57:44 -0300

    Udemy backend v01.003.001 React Keeper App Final Part

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,44 @@
-import React from "react";
+import React, {useState} from "react";
 import reactDom from "react-dom";
 import Heading from "./Heading";
 import Note from "./Note";
 import Footer from "./Footer";
-import notes from "../notes";
+//import notes from "../notes";
+import CreateArea from "./CreateArea";
 
 function App() {
-return ( 
-	<div>
-		<Heading />
-		{notes.map((noteItem) => (
-			 <Note		
-			key={noteItem.key}
-			title={noteItem.title}
-			content={noteItem.content}
-			/>
-		))}
-		<Footer />
-	</div>
-);
+	const [notes, setNotes] = useState([]);
+
+	function addNote(newNote) {
+		setNotes(prevNotes => {
+			return [...prevNotes, newNote];
+		});
+	}
+
+	function deleteNote(id) {
+		setNotes(prevNotes => {
+			return prevNotes.filter((noteItem, index) => {
+				return index !== id;
+			});
+		});
+	}
+
+	return ( 
+		<div>
+			<Heading />
+			<CreateArea onAdd={addNote} />
+			{notes.map((noteItem, index) => (
+				 <Note
+					key={index}
+					id={index}
+					title={noteItem.title}
+					content={noteItem.content}
+					onDelete={deleteNote}
+				/>
+			))}
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
